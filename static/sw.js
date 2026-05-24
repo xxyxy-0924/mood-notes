7a3f6a18d4ef726f57e81ca041c670fedcd33d34
zhuyihao0924-ctrl <zhuyihao0924@gmail.com>
2026-05-25 00:51:56 +0800

    Add service worker

---
+++ static/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'mood-notes-pwa-v1';
+const APP_SHELL = [
+  '/',
+  '/manifest.webmanifest',
+  '/static/icon.svg',
+  '/static/maskable-icon.svg',
+  '/static/splash.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+  const url = new URL(request.url);
+
+  if (request.method !== 'GET' || url.pathname.startsWith('/api/') || url.pathname === '/login') {
+    return;
+  }
+
+  event.respondWith(
+    fetch(request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
+        return response;
+      })
+      .catch(() => caches.match(request).then(cached => cached || caches.match('/')))
+  );
+});
